9e75ed51296b347adc41f860d4d902e82ff903e2
angel <angel@192.168.1.109>
2025-10-09 11:37:54 +0200

    Preparacion para empezar a trabajar

---
+++ src/main/java/com/mycompany/projecte1_grup4/Projecte1_Grup4.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.projecte1_grup4;
+
+/**
+ *
+ * @author angel
+ */
+public class Projecte1_Grup4 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
