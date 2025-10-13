674b7dfa4c31b6d3333f119d0be924014b7a53f7
Angel <rjurado@alumnat.copernic.cat>
2025-10-13 10:10:16 +0200

    Constructors

--- src/main/java/entities/Tauler.java
+++ src/main/java/entities/Tauler.java
@@ -12,10 +12,27 @@ public class Tauler {
     
     
     /**************    CONSTRUCTORS.    **************
-     * Constructor per defecte que crea un tauler de 6 files × 7 columnes.
-     * Aquestes són les dimensions estàndard del joc Conecta 4.
+     * Constructors per crear un tauler de Conecta 4.
+     * Per defecte: 6 Files × 7 Columnes.
+     * També es pot especificar un nombre personalitzat de files i columnes.
+     *
+     * @param files Nombre de files del tauler
+     * @param columnes Nombre de columnes del tauler
      */
     
+    public Tauler() {
+        this.files = 6;
+        this.columnes = 7;
+        inicialitzarTauler(); // Crida el mètode
+    }
+    
+    public Tauler(int files, int columnes) {
+        this.files = files;
+        this.columnes = columnes;
+        inicialitzarTauler(); // Crida el mètode
+    }
+    
+    private void inicialitzarTauler() {}
     
     
     
