72d50eaed6ccc61e5c7fe559d198088cb68a78c2
Angel <rjurado@alumnat.copernic.cat>
2025-10-13 09:52:46 +0200

    Canvis

--- src/main/java/entities/Tauler.java
+++ src/main/java/entities/Tauler.java
@@ -2,8 +2,37 @@ package entities;
 
 /**
  * @author Ángel Jurado Herruzo
- */
+ */ 
 
 public class Tauler {
+   
+    private Casella[][] caselles;   // Matriu bidimensional
+    private int files;              // Nombre de files: 6
+    private int columnes;           // Nombre de columnes: 7
+    
+    
+    /**************    CONSTRUCTORS.    **************
+     * Constructor per defecte que crea un tauler de 6 files × 7 columnes.
+     * Aquestes són les dimensions estàndard del joc Conecta 4.
+     */
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     
 }
