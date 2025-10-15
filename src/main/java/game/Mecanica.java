43f846425b84c6b0150347e74166802f360b163c
angel <angel@192.168.1.109>
2025-10-15 09:17:00 +0200

    Inicio de la clase Mecanica, Nada de texto

--- src/main/java/game/Mecanica.java
+++ src/main/java/game/Mecanica.java
@@ -9,5 +9,18 @@ package game;
  * @author angel
  */
 public class Mecanica {
+    private int tornActual = 1;
+    private int columnaNovaFicha;
     
+    public void incrementarTorn(){
+        tornActual++;
+    }
+    public int getTornActual() {
+    return tornActual;
+}
+    public void afegirFichaMecanica(){
+}
+    public int getColumnaNovaFicha() {
+    return columnaNovaFicha;
+}
 }
